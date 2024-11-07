b8f153dcbf20f7ab2bc38d3fe00dd98a9ac7b2fd
YourAverageMo <Yilmaz@arizona.edu>
2024-11-07 14:03:24 -0700

    changing text content & basic setup

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log(`Hello`);
+// console.log(`I like Pizza`);
+
+// window.alert(`This is an alert`)
+
+document.getElementById("myH1").textContent = `Hello`
+document.getElementById("myP").textContent = `I like pizza`
+
+let x = 123;
+console.log(x)

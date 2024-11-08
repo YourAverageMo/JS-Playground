a8badcfd2f4a645ce404878181abf2732c205fa4
YourAverageMo <Yilmaz@arizona.edu>
2024-11-07 17:42:33 -0700

    simple counter program

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-// console.log(`Hello`);
-// console.log(`I like Pizza`);
+const decreaseBtn = document.getElementById(`decreaseBtn`);
+const resetBtn = document.getElementById(`resetBtn`);
+const increaseBtn = document.getElementById(`increaseBtn`);
+const countLabel = document.getElementById(`countLabel`);
+let count = 0;
 
-// window.alert(`This is an alert`)
-
-document.getElementById("myH1").textContent = `Hello`
-document.getElementById("myP").textContent = `I like pizza`
-
-let x = 123;
-console.log(x)
+increaseBtn.onclick = function () {
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function () {
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function () {
+    count = 0;
+    countLabel.textContent = count;
+}

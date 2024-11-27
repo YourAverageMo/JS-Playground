b7ab128cf3390b8c3f57396eb9091354cc489f71
YourAverageMo <Yilmaz@arizona.edu>
2024-11-26 22:45:30 -0700

    simple moving box game with keyboard and event listeners

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const display = document.getElementById(`display`)
 
-function appendToDisplay(input) {
-    display.value += input
-}
-function clearDisplay() {
-    display.value = ``
-}
-function calculate(input) {
-    try {
-        display.value = eval(display.value)
-    } catch (error) {
-        display.value = `Error`
+const moveAmount = 100
+
+let x = 0
+let y = 0
+
+const myBox = document.getElementById(`myBox`)
+
+document.addEventListener("keydown", event => {
+    if (event.key.startsWith(`Arrow`)) {
+        switch (event.key) {
+            case "ArrowUp":
+                myBox.textContent = "☝🏽"
+                myBox.style.backgroundColor = `tomato`
+                y -= moveAmount
+                break;
+            case "ArrowDown":
+                myBox.textContent = "👇🏽"
+                myBox.style.backgroundColor = `tomato`
+                y += moveAmount
+                break;
+            case "ArrowLeft":
+                myBox.textContent = "👈🏽"
+                myBox.style.backgroundColor = `tomato`
+                x -= moveAmount
+                break;
+            case "ArrowRight":
+                myBox.textContent = "👉🏽"
+                myBox.style.backgroundColor = `tomato`
+                x += moveAmount
+                break;
+        }
+
+        myBox.style.top = `${y}px`
+        myBox.style.left = `${x}px`
     }
-}
+})
+document.addEventListener("keyup", event => {
+    myBox.textContent = "😜"
+    myBox.style.backgroundColor = `lightblue`
+})    
